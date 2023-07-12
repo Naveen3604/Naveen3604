e8ee9d8b7ec814c3cd246f74066c6e6e09db40ca
Naveen3604 <139300297+Naveen3604@users.noreply.github.com>
2023-07-12 05:36:01 +0330

    Create README.py

---
+++ README.py
@@ -0,0 +1,6 @@
+def About_Myself(name,Education,Skills,Native):
+    print("HELLO")
+    name='Naveen P'
+    Education=' Pursuing 3rd year ECE at Paavai Engineering College'
+    Skills='Python, Web Designing'
+    return
